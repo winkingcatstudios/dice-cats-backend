a454a0722598e70ad32af3b1d958bb275ac80f9d
winkingcat <dankercher@gmail.com>
2022-08-02 14:22:04 -0400

    fixed bug not accepting token

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
   next();
 });
 
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,18 +4,19 @@ const HttpError = require("../models/http-error");
 const getJWTPrivateKey = require("../dev-files/dev-files").getJWTPrivateKey;
 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.spit(" ")[1]; // Authorization: "Bearer TOKEN"
-
-    if (!token) {
-      throw new Error("Authentication failed");
+    if (req.method === 'OPTIONS') {
+      return next();
     }
-
-    const decodedToken = jwt.verify(token, getJWTPrivateKey());
-    req.userData = { userId: decodedToken.userId };
-    next();
-  } catch (err) {
-    const error = new HttpError("Authentication failed", 401);
-    return next(error);
-  }
-};
+    try {
+      const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+      if (!token) {
+        throw new Error('Authentication failed!');
+      }
+      const decodedToken = jwt.verify(token, getJWTPrivateKey());
+      req.userData = { userId: decodedToken.userId };
+      next();
+    } catch (err) {
+      const error = new HttpError('Authentication failed!', 401);
+      return next(error);
+    }
+  };
